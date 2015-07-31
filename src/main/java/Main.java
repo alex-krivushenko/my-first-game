607471e8a9767dc51e2547702d839f1f8b5f272b
Alexander Krivushenko <alex.krivushenko@gmail.com>
2015-07-31 18:17:47 +0300

    First friday commit. Nothing work.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
+import frontend.AuthServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 /**
- * Created by Александр on 31.07.2015.
+ * Created by Alexander on 31.07.2015.
  */
 public class Main {
+    public static void main(String[] args) throws Exception {
+        AuthServlet frontend = new AuthServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(frontend), "/authform");
+
+        Server server = new Server(8081);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
 
 }
